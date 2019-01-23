4b6853c202167b593d312998077359f1c197fdb8
rainboy <rainboylvx@qq.com>
2019-01-23 23:54:31 +0800

    fix webhook

--- routes/methods/update.js
+++ routes/methods/update.js
@@ -3,12 +3,12 @@
 module.exports = async (ctx,next)=>{
 
     //git的  地址,c_book_path
-    let {token} = ctx.request.body
-    if( token === C.token){
-        await U.loadAllCatalog()    //更新目录
-        ctx.body = {status:0}
+    let {password} = ctx.request.body
+    if( password === C.token){
+        U.loadAllCatalog()    //更新目录
+        ctx.body = "更新成功"
         return
     }
     ctx.status = 500
-    ctx.body = "invalue token"
+    ctx.body = "不正确的token"
 }
