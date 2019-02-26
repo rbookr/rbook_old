02450cb06e1f199faf929fac33f9e12b38215f0d
rainboy <rainboylvx@qq.com>
2019-02-26 14:26:18 +0800

    fix 更新触发

--- routes/methods/update.js
+++ routes/methods/update.js
@@ -1,11 +1,14 @@
 /* 接收触发,更新git */
 
+
 module.exports = async (ctx,next)=>{
 
     //git的  地址,c_book_path
     let {password} = ctx.request.body
 
     if( password === C.token){
+        /* 更新git */
+        await U.git.pull_master()
         await redis.loadCatalog(true);
         ctx.body = "更新成功 " + (new Date()).toString()
         return
